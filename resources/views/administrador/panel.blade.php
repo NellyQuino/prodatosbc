4072bb6bb797e29034013bcbd702ddf8bd354a7f
JonatanJimenez <jonatan.jimenez17@tectijuana.edu.mx>
2022-03-28 18:59:21 -0700

    Update panel.blade.php

--- resources/views/administrador/panel.blade.php
+++ resources/views/administrador/panel.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.template_admin')
 @section('content')
 
+<style>
+        .active a {
+            color: #BD3284 !important;
+            font-weight: bold !important;
+            text-decoration: none !important;
+        }
+</style>
+
 <div class="text-left col-sm-8">
     <label style="color: #848483 ;font-size:250%;font-family:inter;" for="">Panel</label>
 </div>
 <div>
+
     <nav class="navbar navbar-expand-lg shadow-sm col-md-12" style="background-color: #059B97" >
             <ul class="navbar-nav">
                 <li  class=" {{ setActive('ejes.index') }} " >
