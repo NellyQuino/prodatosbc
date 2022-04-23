0540f4f6c3aea981bb53576ba73a513765adc703
JonatanJimenez <jonatan.jimenez17@tectijuana.edu.mx>
2022-04-22 18:18:51 -0700

    pdf nueva version

--- resources/views/administrador/sujetos/pdf.blade.php
+++ resources/views/administrador/sujetos/pdf.blade.php
@@ -6,6 +6,22 @@
 
     <link href="css/pdf.css" rel="stylesheet" type="text/css">
     
+    <style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+  table-layout: fixed;
+}
+
+th, td {
+    width: 100px;
+    word-wrap: break-word;
+}
+
+th{
+  text-align:left;
+}
+</style>
   </head>
   
   <body>
@@ -13,9 +29,9 @@
 
       <div class="container">
       
-      <img src="{{ storage_path('/app/public/logos/1647372823-new.jpg') }}" style="width: 300px; height: 150px">
+      <img src="{{ storage_path('/app/public/logos/1647372823-new.jpg') }}" style="width: 300px; height: 150px;">
   
-      <h1>Reporte de {{$user->username}}</h1>
+      <!-- <h1>Reporte de {{$user->username}}</h1> -->
 
       </div>
 
@@ -33,12 +49,32 @@
       </tbody>
     </table> -->
     
-    <table>
-      <!-- <tr>
-        <td>Nombre:</td>
-        <td>{{$user->username}}</td>
-      </tr> -->
-      @foreach ($compromisos as $compromiso)
+    <h3 style="text-align: right">Fecha del reporte: 20/04/2022 </h3>
+    <table style="width:100%">
+  <tr>
+    <th style="width:75%">Nombre del sujeto obligado:	</th>
+    <th>ID</th>
+  </tr>
+  <tr>
+    <td>{{$user->username}}</td>
+    <td>{{$user->number_user}}</td>
+  </tr>
+  </table>
+  <br> 
+  <table style="width:100%">
+  <tr>
+    <th style="width:60%">Servidor público que reporta:	</th>
+    <th>Correo electrónico: </th>
+  </tr>
+  <tr>
+    <td>{{$user->slug}}</td>
+    <td>{{$user->email}}</td>
+  </tr>
+    </table>
+
+    <p>Ejes, estrategias y líneas de acción que se reportan:</p>
+    <!-- <table style="width:100%">
+    @foreach ($compromisos as $compromiso)
         <tr>
           <td id="titulo">Compromisos: </td>
           <td id="subtitulo">{{$compromiso->action_plan}}</td>
@@ -56,6 +92,70 @@
         <p style="page-break-before: always;"></p>
         @endif
       @endforeach
+    </table> -->
+    
+    <table style="width:100%">
+    @foreach ($compromisos as $compromiso)
+  <tr>
+    <th colspan="2">Eje</th>
+    <th colspan="2">Estrategia</th>
+    <th colspan="2">Línea de acción {{$compromiso->accion_id}}</th>
+</tr>
+@endforeach
+<!-- <tr>
+<td colspan="2">Eje</td>
+    <td colspan="2">Estrategia</td>
+    <td colspan="2">Línea de acción</td>
+</tr>
+<tr>
+<td colspan="2">Eje</td>
+    <td colspan="2">Estrategia</td>
+    <td colspan="2">Línea de acción</td>
+</tr> -->
+  <tr>
+    <th style="width:20%">Total ejes:</th>
+    <td></td>
+    <th style="width:20%">Total estrategias:</th>
+    <td></td>
+    <th style="width:30%">Total líneas de acción:</th>
+    <td></td>
+  </tr>
     </table>
+
+  <br>
+
+    @foreach ($compromisos as $compromiso)
+    <table style="width:100%">
+  <tr>
+    <th>Eje</th>
+    <th>Estrategia</th>
+    <th>Línea de acción</th>
+</tr>
+<tr>
+    <td>Jill</td>
+    <td>Jill</td>
+    <td>Jill</td>
+</tr>
+<tr>
+    <th colspan="3">Descripción de la actividad </th>
+</tr>
+<tr>
+    <td colspan="3">{{$compromiso->action_plan}}</td>
+</tr>
+<tr>
+    <th colspan="2">Fecha en que se adquirió el compromiso:</th>
+    <td>{{$compromiso->created_at}}</td>
+</tr>
+<tr>
+    <th colspan="2">Fecha de entrega de la evidencia:</th>
+    <td>21/04/2022</td>
+</tr>
+<tr>
+    <td colspan="2"></td>
+    <td><b>Estatus:</b> {{$compromiso->detail}}</td>
+</tr>
+    </table>
+    <br>
+    @endforeach
   </body>
 </html>
