756fa557f6c79b5ec06cb8d26f4c2c6707c70320
Josue-cloudU <75402445+Josue-cloudU@users.noreply.github.com>
2022-07-13 18:46:27 -0700

    .

--- resources/views/sujeto/index.blade.php
+++ resources/views/sujeto/index.blade.php
@@ -2,29 +2,26 @@
 @section('content')
 <h1>Información</h1>
 <div class="row" style="margin-bottom: 3rem; margin-top: 1rem; justify-content:center">
-    <div class="col-sm-3">
-        <div class="card text-white" style="max-width: 18rem; background-color: #d270ab;">
-        <div class="card-header">Implementación</div>
+    <div class="col-md-4">
+        <div class="card text-white" style="max-width: 26rem; background-color: #c54793">
+        <div class="card-header"><h5 class="card-title">Implementación</h5></div>
         <div class="card-body">
-            <h5 class="card-title">Primary card title</h5>
-            <p class="card-text">En este formato los sujetos obligados responsables seleccionarán los ejes y las acciones que desarrollarán durante el ejercicio anual; asimismo, remitirán la descripción de las actividades concretas que llevarán a cabo en cada línea de acción. </p>
+            <p class="card-text" >En este formato los sujetos obligados responsables seleccionarán los ejes y las acciones que desarrollarán durante el ejercicio anual; asimismo, remitirán la descripción de las actividades concretas que llevarán a cabo en cada línea de acción. </p>
         </div>
         </div>
     </div>
-    <div class="col-sm-3">
-        <div class="card text-white" style="max-width: 18rem; background-color: #cc5b9f">
-        <div class="card-header">ABC (altas, bajas, cambios)</div>
+    <div class="col-md-4">
+        <div class="card text-white" style="max-width: 26rem; background-color: #c54793">
+        <div class="card-header"><h5 class="card-title">ABC (altas, bajas, cambios)</h5></div>
         <div class="card-body">
-            <h5 class="card-title">Primary card title</h5>
             <p class="card-text">En este formato, los sujetos obligados que así lo requieran podrán modificar la ruta de implementación inicial, ya que permite seleccionar ejes, estrategias y/o acciones adicionales, dar de baja alguno de los previamente seleccionados y/o cambiar la descripción de las actividades propuestas. </p>
         </div>
         </div>
     </div>
-    <div class="col-sm-3">
-        <div class="card text-white" style="max-width: 18rem; background-color: #c54793">
-        <div class="card-header">Seguimiento</div>
+    <div class="col-md-4">
+        <div class="card text-white" style="max-width: 26rem; background-color: #c54793">
+        <div class="card-header"><h5 class="card-title">Seguimiento</h5></div>
         <div class="card-body">
-            <h5 class="card-title">Primary card title</h5>
             <p class="card-text">Este formato será la vía para que los responsables remitan al Instituto, las evidencias que respalden las acciones y actividades realizadas a lo largo del ejercicio.
                 Las evidencias deberán estar contenidas en archivos con formato .rar, .zip y/o .7z.  
             </p>
