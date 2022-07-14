5f8e2a8c3e02cfe3cc6773af0a6511393d049c55
JonatanJimenez <jonatan.jimenez17@tectijuana.edu.mx>
2022-07-13 21:13:53 -0700

    color

--- resources/views/administrador/fases/index.blade.php
+++ resources/views/administrador/fases/index.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 <div class="btn-group col-xl-12 my-3" role="group" aria-label="Basic radio toggle button group">
         <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio1">Implementación</label>
+        <label class="btn btn-outline-prim" for="btnradio1">Implementación</label>
 
         <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio2">ABC</label>
+        <label class="btn btn-outline-prim" for="btnradio2">ABC</label>
 
         <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio3">Seguimiento</label>
+        <label class="btn btn-outline-prim" for="btnradio3">Seguimiento</label>
     </div>
 <div class="row justify-content-end">
     <div class="text-left">
